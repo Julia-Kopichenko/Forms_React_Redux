c848ca67bf9bcac0848fc8fa13311a70710df225
YuliyaKopichenko <j.titorenko@gmail.com>
2021-12-22 16:35:36 +0300

    add istruction from 171 video

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         use: [
+          // loading images
           {
             loader: "file-loader",
             options: {
               outputPath: "images",
               name: "[name]-[sha1:hash:7].[ext]",
+            },
+          },
+        ],
+      },
+      // loading fonts
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts",
+              name: "[name].[ext]",
             },
           },
         ],
