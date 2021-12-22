ea48f43eb1f042744580bf2a5c49130a6e496410
YuliyaKopichenko <j.titorenko@gmail.com>
2021-12-22 12:20:07 +0300

    add babel

---
+++ build/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var App = function App() {
+  return /*#__PURE__*/React.createElement("p", null, "Hello");
+};
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
