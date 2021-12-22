c8ff4a0b733225f133b4c4e3846e3df3e7d03ce4
YuliyaKopichenko <j.titorenko@gmail.com>
2021-12-22 17:29:08 +0300

    add istruction from 178 video

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -39,12 +40,12 @@ module.exports = {
       // loading css
       {
         test: /\.(css)$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       // loading SASS/SCSS
       {
         test: /\.(s[ca]ss)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -54,6 +55,9 @@ module.exports = {
       title: "Hello World",
       buildTime: new Date().toISOString(),
       template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main-[hash:8].css",
     }),
   ],
 };
